c4387283dd45658daa0575bead3e3263039fa254
FacundoThibaut1010 <thibautfacundo7@gmail.com>
2026-07-02 18:42:42 -0300

    detalle de producto y navegacion por card

--- src/app/modules/products/pages/product-detail/product-detail.ts
+++ src/app/modules/products/pages/product-detail/product-detail.ts
@@ -5,16 +5,19 @@ import { Producto } from '../../interfaces/producto.interface';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ProductNotFound } from "../../components/product-not-found/product-not-found";
+import { CartService } from '../../../cart/services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail-page',
   templateUrl: './product-detail.html',
-  imports: [ProgressSpinnerModule, ProductNotFound],
+  imports: [ProgressSpinnerModule, ProductNotFound, CurrencyPipe],
 })
 export class ProductDetailPage implements OnInit {
 
   private activatedRoute = inject(ActivatedRoute);
   private productService = inject(ProductService);
+  readonly cartService = inject(CartService);
   readonly product = signal<Producto | null>(null);
   readonly error = signal<HttpErrorResponse | null>(null);
   readonly loading = signal<boolean>(false);
