50b493a5e6cdbef0d88fc8a4991c13682352da63
mfolco <marcofolco28@gmail.com>
2026-07-04 10:49:27 -0300

    feat(i18n): configurar locale español para formateo de precios y fechas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -9,6 +11,8 @@ import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { providePrimeNG } from 'primeng/config';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import Aura from '@primeuix/themes/aura';
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +25,7 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
+    { provide: LOCALE_ID, useValue: 'es' },
     MessageService,
     ConfirmationService,
   ],
